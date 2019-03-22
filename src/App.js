e909c950e516ad8478393d63344e9936bc710a38
xa-xa-xa <28684394+xa-xa-xa@users.noreply.github.com>
2019-03-22 10:28:14 -0700

    initial commit: just started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Todos from './components/Todos';
+import Header from './components/layout/Header';
+
 import './App.css';
 
 class App extends Component {
-  render() {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Clean room',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Go to Avengers',
+        completed: false
+      },
+    ]
+  }
+
+  // Toggle complete
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    })
+  }; 
+
+  // Delete Todo
+  delTodo = (id) => {
+    this.setState({
+      todos: [ ...this.state.todos.filter(todo =>
+        todo.id !== id) ]
+    });
+  }
+  
+
+  
+  render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <Todos todos={ this.state.todos } markComplete={ this.markComplete } delTodo={ this.delTodo } />
+
+
       </div>
     );
   }
